06f333d0cb8a24479b3a22aa0e49edca50a864bd
rmiekina <r.m.wp@wp.pl>
2016-05-31 20:26:09 +0200

    add test and generate reports

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -4,6 +4,19 @@ describe('app', function () {
     var answer = window.app;
 
     describe('calculateArea', function () {
+        //Checking function inputs
+
+
+        it('should return area 0 & message "Big null"', function () {
+            expect(answer.calculateArea(0,0,0,'Ok', 'Fail')).toEqual({area: 0, message:'Big null'});
+
+        });
+        it('should return area of figure & message', function () {
+            expect(answer.calculateArea(10, 'a', 10, 'Ok', 'Fail')).toBe(false);
+            expect(answer.calculateArea(6, 'x', 30, 'Ok', 'Fail')).toBe(false);
+            expect(answer.calculateArea(6,1,'','Ok', 'Fail')).toEqual(false);
+            expect(answer.calculateArea("",1,'','Ok', 'Fail')).toEqual(false);
+        });
         it('should return area of figure & message', function () {
             expect(answer.calculateArea(6,1,3,'Ok', 'Fail')).toEqual({area: 3, message: 'Ok'});
             expect(answer.calculateArea(2,6,6,'Ok', 'Fail')).toEqual({area: -6, message: 'Fail'});
@@ -12,16 +25,7 @@ describe('app', function () {
             expect(answer.calculateArea(5,1,1,'Ok', 'Fail')).toEqual({area: 4, message: 'Ok'});
 
         });
-        it('should return area of figure & message', function () {
-            expect(answer.calculateArea(6,"x",3,'Ok', 'Fail')).toEqual(false);
-            expect(answer.calculateArea(6,1,'','Ok', 'Fail')).toEqual(false);
-            expect(answer.calculateArea("",1,'','Ok', 'Fail')).toEqual(false);
-        });
-
-        it('should return area 0 & message "Big null"', function () {
-            expect(answer.calculateArea(0,0,0,'Ok', 'Fail')).toEqual({area: 0, message:'Big null'});
-
-        });
-
+       
+       
     });
 });
